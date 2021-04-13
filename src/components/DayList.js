4644beda7b521edf02f3857b9f3d100895c62a75
C-Shi <cshi@ualberta.ca>
2021-04-12 21:04:25 -0600

    Add DayList to App

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,12 +6,12 @@ export default function DayList (props) {
     <ul>
       {props.days.map(day => {
         return (
-          <li key={day.id}>
-            <DayListItem name={day.name} 
-              spots={day.spots} 
-              selected={day.name === props.day}
-              setDay={props.setDay}  />
-          </li>
+          <DayListItem 
+            key={day.id}
+            name={day.name} 
+            spots={day.spots} 
+            selected={day.name === props.day}
+            setDay={props.setDay}  />
           )
       })}
     </ul>
